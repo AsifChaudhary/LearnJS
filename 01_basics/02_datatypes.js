2e029f449c4b8eadd21bd2be85a0ed8b4634b4f8
AsifChaudhary <140884330+AsifChaudhary@users.noreply.github.com>
2024-01-09 16:55:52 +0000

    symbol

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -6,3 +6,20 @@ console.log(null==0)
 console.log("2" > 8)
 console.log("2" === 2)
 
+
+
+//  Primitive data type
+
+/*
+7 types :  String, Number, Boolean, null, undefined, Symbol, BigInt
+
+
+
+
+*/
+// Refrence type(Non primitive)
+// Array,Object, Functions
+
+
+const id=Symbol("123")
+console.log(id)
