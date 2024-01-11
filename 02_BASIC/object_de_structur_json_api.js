1563483fba243f575a94521243ea287f86455adf
Maddy <786.asifchaudhary@gmail.com>
2024-01-11 17:07:54 +0530

    object de structure

---
+++ 02_BASIC/object_de_structur_json_api.js
@@ -0,0 +1,16 @@
+const course = {
+
+coursename:"Java script",
+fees:999,
+duration:"6 months",
+"trainer name":"John Doe",
+
+
+}
+
+
+
+const {coursename:cname} = course
+
+console.log(course.coursename);
+console.log(cname);
