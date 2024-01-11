d224c281305bca9437dd954cc1fe92c2f9a9dab0
AsifChaudhary <140884330+AsifChaudhary@users.noreply.github.com>
2024-01-11 06:19:51 +0000

    object 1 lecture completed

---
+++ 02_BASIC/objects.js
@@ -0,0 +1,36 @@
+const sym = Symbol("key1")
+ 
+ const userobj ={
+
+    
+    name:'Asif',
+    age:24,
+    email:"asif@gmail.com",
+    [sym]:"Mykey1",
+ }
+
+//  console.log(userobj.name)
+//  console.log(userobj['age'])
+//  console.log(typeof userobj[sym])
+
+//  userobj.name = "maddy"
+//  console.log(userobj.name)
+
+//  Object.freeze(userobj)  // freeze is used for restriction when we update value in obj
+//  userobj.name = "khan"
+
+ console.log(userobj);
+
+ userobj.greeting = function(){
+    console.log("hello i am greeting") }
+
+
+
+ console.log(userobj.greeting());
+
+ userobj.greeting2 = function(){
+    console.log(`hello i am greeting, ${this.name}`) }
+
+    console.log(userobj.greeting2());
+
+
