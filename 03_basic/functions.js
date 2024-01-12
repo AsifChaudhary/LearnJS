b8be5edcbff71f275fa0245438e8c99ee99c5fe7
Maddy <786.asifchaudhary@gmail.com>
2024-01-12 13:52:19 +0530

    this and arrow function

---
+++ 03_basic/functions.js
@@ -0,0 +1,24 @@
+function sayname(){
+    console.log("A")
+    console.log("S")
+    console.log("I")
+    
+    console.log("F")
+}
+
+sayname()
+
+sayname()
+// sayname()
+// sayname()
+// sayname()
+
+
+const abc = () =>
+{
+    console.log("helfffflo")
+}
+
+abc()
+
+// const hh = functio
---
+++ 03_basic/this&arrow.js
@@ -0,0 +1,15 @@
+// const chai = ()=>{
+//     console.log('hello')
+// }
+
+// chai()
+
+
+
+(function hello(){
+    console.log("Hello");   // named IIFE   (Immediately invocked function Expression)
+})();
+
+(()=> {
+    console.log("bhai");   // Unnamed IIFE   (Immediately invocked function Expression)
+})();
