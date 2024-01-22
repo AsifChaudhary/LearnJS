1b415c6d3b6e8a2a5e0857efe47232e00fe93386
Maddy <786.asifchaudhary@gmail.com>
2024-01-22 20:47:04 +0530

    add foreach loop

---
+++ 04_iteration/high_loop.js
@@ -0,0 +1,23 @@
+arr=['asif','jafar','ritesh','danish']
+
+
+// arr.forEach(function(val){
+//     console.log(val);
+// });
+
+// arr.forEach(ele=>{
+
+//     console.log(ele);
+// })
+
+
+// const abc = ()=>{
+//     console.log('abc');
+// }
+
+// abc();
+
+
+arr.forEach((ele,inde)=>{
+    console.log(ele,inde);
+});
