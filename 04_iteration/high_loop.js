bf32831c939800877e291364f473afcd1e384dc3
Maddy <786.asifchaudhary@gmail.com>
2024-01-22 23:52:27 +0530

    reduce

--- 04_iteration/high_loop.js
+++ 04_iteration/high_loop.js
@@ -40,9 +40,48 @@ const mynum =[1,2,3,6,5,4,7,5,1]
 // })
 // console.log(newNum);
 
-const newNum = mynum.map((num)=> num*10)
-.map((num)=>num+1)
-.filter((num)=>num>40)
+// const newNum = mynum.map((num)=> num*10)
+// .map((num)=>num+1)
+// .filter((num)=>num>40)
 
 
-console.log(newNum);
+// console.log(newNum);
+
+const Mynum =[1,2,3,4]
+// const newarray = Mynum.filter((ele)=>ele >2)
+
+// const newarray = Mynum.map((ele)=>ele >2)
+
+const newarr = Mynum.reduce((acc,curr)=>{
+    return acc+curr
+},0)
+console.log(newarr);
+
+
+const shoppingcart =[
+
+    {
+
+        item1:"Condom",
+        price:1000,
+        quantity1:2
+    },
+    {item2:"Vibrator",
+    price:2000,
+    quantity2:3},
+
+    {
+    item3:"Microwave",
+    price:3000,
+    quantity3:4
+}
+];
+
+const total = shoppingcart.reduce((acc,curr)=>{
+
+    return acc+curr.price
+
+},0)
+
+console.log(total);
+
