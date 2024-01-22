d90593ec295bd216359bed32a0bfb090d5285404
Maddy <786.asifchaudhary@gmail.com>
2024-01-22 23:09:28 +0530

    Map and filter

--- 04_iteration/high_loop.js
+++ 04_iteration/high_loop.js
@@ -18,6 +18,31 @@ arr=['asif','jafar','ritesh','danish']
 // abc();
 
 
-arr.forEach((ele,inde)=>{
-    console.log(ele,inde);
-});
+// arr.forEach((ele,inde)=>{
+//     console.log(ele,inde);
+// });
+
+
+
+const mynum =[1,2,3,6,5,4,7,5,1]
+
+// const aa = mynum.filter((num)=>{
+//     return num>4
+// })
+
+// console.log(aa)
+
+
+// const newNum= []
+// mynum.map((ele)=>{
+//     (ele > 5)?newNum.push(ele):console.log("Byyeeee");
+
+// })
+// console.log(newNum);
+
+const newNum = mynum.map((num)=> num*10)
+.map((num)=>num+1)
+.filter((num)=>num>40)
+
+
+console.log(newNum);
